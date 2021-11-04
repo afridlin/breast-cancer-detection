62a77863ba45c3cdd900c2d95418136505750cb6
Alexander <alex.fridlin@gmail.com>
2021-11-04 16:57:49 +0100

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+# Breast Cancer Detection
+# Based on this video: https://www.youtube.com/watch?v=NSSOyhJBmWY
+
+# Import libraries
+# A library is a collection of resources, like pre-written code, used by computer programs
+# (https://en.wikipedia.org/wiki/Library_(computing))
+
+# NumPy is a library that offers mathematical functions and adds support for vectors and matrices
+import numpy
+
+# Pandas is a library for managing and analysing data
+import pandas
+
+# Matplotlib is a library for creating graphical visualizations
+import matplotlib.pyplot as plot
+
+# Seaborn is a library for data visualization based on Matplotlib (see above)
+import seaborn 
+
+# Jump to 4:30 in the video: https://www.youtube.com/watch?v=NSSOyhJBmWY&t=270s
+
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# run this script once to set up your development environment for the breast cancer detection
+
+# install required python modules
+pip3 install numpy pandas matplotlib seaborn
+
+# clone the git repository including some basic code and the breast cancer data
+git clone 
